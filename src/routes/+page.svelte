b40d2ff7fb49019d6bc69b312a337b6940d03fa0
Jacob Phillips <jifalops@gmail.com>
2024-11-17 02:11:30 -0500

    add a simple light/dark theme and support @import for postcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,9 @@
 	import SvelteLogo from 'virtual:icons/logos/svelte-icon';
 </script>
 
-<h1>Welcome to SvelteKit <SvelteLogo class="m-2 inline w-8" /></h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<article class="p-2">
+	<h1>Welcome to SvelteKit <SvelteLogo class="m-2 inline w-8" /></h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit#">svelte.dev/docs/kit</a> to read the documentation.
+	</p>
+</article>
