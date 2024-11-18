b5851e6ce97fa0902f4c9daf91fa894642d6b018
jifalops <jifalops@users.noreply.github.com>
2024-11-18 19:11:13 +0000

    Deploy preview for PR 5 🛫

---
+++ pr-preview/pr-5/_app/immutable/chunks/paths.DJ37Cq5D.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_c6spvb)==null?void 0:s.base)??"/svelte-static/pr-preview/pr-5";var e;const a=((e=globalThis.__sveltekit_c6spvb)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ pr-preview/pr-5/_app/immutable/entry/start.CnfgYdeO.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DqydXA7Q.js";export{t as start};
