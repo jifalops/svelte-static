2fda724b43bee6849afa09f76cae0ba39ffcf229
Jacob Phillips <jifalops@gmail.com>
2024-11-30 13:00:52 -0500

    use send_to when using multiple gtag services

--- src/lib/ad_campaign/service_google_ads.ts
+++ src/lib/ad_campaign/service_google_ads.ts
@@ -10,15 +10,18 @@ declare global {
  * Google Ads (AdWords) adapter for the ad campaign service.
  */
 export class AdCampaignServiceGoogleAds extends AdCampaignService {
-  constructor(gtagId: string) {
+  private trackingId: string;
+
+  constructor(trackingId: string) {
     super();
+    this.trackingId = trackingId;
     const script = document.createElement('script');
     script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
     document.head.appendChild(script);
     window.dataLayer = window.dataLayer || [];
     this.gtag('js', new Date());
-    this.gtag('config', gtagId);
+    this.gtag('config', trackingId);
   }
 
   private gtag(..._: any[]): void {
@@ -26,9 +29,9 @@ export class AdCampaignServiceGoogleAds extends AdCampaignService {
   }
 
   trackClick(targetId: string): void {
-    this.gtag('event', 'click', { targetId });
+    this.gtag('event', 'click', { targetId, send_to: this.trackingId });
   }
   trackConversion(label: string, value?: number): void {
-    this.gtag('event', 'conversion', { label, value });
+    this.gtag('event', 'conversion', { label, value, send_to: this.trackingId });
   }
 }
--- src/lib/telemetry/service_google_analytics.ts
+++ src/lib/telemetry/service_google_analytics.ts
@@ -10,8 +10,11 @@ declare global {
  * Google Analytics 4 adapter for the telemetry service.
  */
 export class TelemetryServiceGoogleAnalytics extends TelemetryService {
+  private measurementId: string;
+
   constructor(measurementId: string) {
     super();
+    this.measurementId = measurementId;
     const script = document.createElement('script');
     script.async = true;
     script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
@@ -27,18 +30,23 @@ export class TelemetryServiceGoogleAnalytics extends TelemetryService {
 
   pageView(path: string): void {
     this.gtag('event', 'page_view', {
-      page_path: path
+      page_path: path,
+      send_to: this.measurementId
     });
   }
 
   event(name: string, params?: Record<string, any>): void {
-    this.gtag('event', name, params);
+    this.gtag('event', name, {
+      ...params,
+      send_to: this.measurementId
+    });
   }
 
   timing(name: string, value: number): void {
     this.gtag('event', 'timing_complete', {
       name,
-      value
+      value,
+      send_to: this.measurementId
     });
   }
 }
